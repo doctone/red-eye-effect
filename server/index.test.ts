4604199c252cd862afdceaba5ce9fffe89039082
Sam James <samjojames@gmail.com>
2023-09-16 15:15:31 +0100

    gitignore & trim test

--- server/index.test.ts
+++ server/index.test.ts
@@ -3,7 +3,6 @@ import request from "supertest";
 import app from ".";
 
 it("runs", async () => {
-  expect(true).toBe(true);
   const res = await request(app).get("/");
   expect(res.body).toMatchObject({ message: "Hello World!" });
 });
